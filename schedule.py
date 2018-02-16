600fe796cb58483913dacba7b46a9e8509f76c76
BoomSyrup <byron4white@gmail.com>
2018-02-16 13:46:34 -0800

    working on class to parse sections and their times

--- schedule.py
+++ schedule.py
@@ -1,26 +1,105 @@
-def get_sec(time_str):
+def parse_minutes(time_str):
     h, m = time_str[:-3].split(':')
     if(time_str[-2:] == "PM"):
-        return int(h) * 60 + int(m) + 720
+        int(h) * 60 + int(m) + 720
     else:
-        return int(h) * 60 + int(m)
+        int(h) * 60 + int(m)
 
-def doesItFit(class1, class2):
 
-def sort(schedule, otherClasses):
+def parse_time(time_str, days):
+    times = []
+    dayInMinutes = 1440
+    dayDict = {
+            "M": 0,
+            "T": dayInMinutes,
+            "W": dayInMinutes * 2,
+            "Th": dayInMinutes * 3,
+            "F": dayInMinutes * 4,
+            "S": dayInMinutes * 5,
+            "U": dayInMinutes * 6}
+    startTime, endTime = time_str.split('-')
+    startTime = parse_minutes(startTime)
+    endTime = parse_minutes(endTime)
+    for day in days:
+        times.insert({startTime+dayDict[day], endTime+dayDict[day]})
+    return times
+
+
+class Classie:
+    def __init__(self, classObj):
+        self.crn = classObj[0]['CRN']
+        self.times = []
+        for oneClass in classObj:
+            tempTime = parse_time(oneClass['time'], oneClass['days'])
+            for time in tempTime:
+                self.times.insert(time)
+
+
+#def doesItFit(class1, class2):
+
+
+
+#def sort(schedule, otherClasses):
 
 
 
 
 def main():
-    classes = [{"time": "10:00 AM-01:50 PM", "CRN": "40157"}, {"time": "08:00 AM-11:50 AM","CRN":"40388"}]
-    for classie in classes:
-        startTime, endTime = classie["time"].split('-')
-        startTime = get_sec(startTime)
-        endTime = get_sec(endTime)
-        classie['startMin'] = startTime
-        classie['endMin'] = endTime
-    print(classes)
+    classExample = [
+        {
+          "CRN": "40203", 
+          "campus": "FH", 
+          "course": "PHYS F004A01", 
+          "days": "TTh", 
+          "desc": "GENERAL PHYSICS (CALC)", 
+          "end": "06/29/2018", 
+          "instructor": "Cascarano", 
+          "room": "4502", 
+          "seats": "28", 
+          "start": "04/09/2018", 
+          "status": "Open", 
+          "time": "10:00 AM-11:50 AM", 
+          "units": "  6.00", 
+          "wait_cap": "10", 
+          "wait_seats": "10"
+        }, 
+        {
+          "CRN": "40203", 
+          "campus": "FH", 
+          "course": "PHYS F004A01", 
+          "days": "T", 
+          "desc": "GENERAL PHYSICS (CALC)", 
+          "end": "06/29/2018", 
+          "instructor": "Low", 
+          "room": "4715", 
+          "seats": "28", 
+          "start": "04/09/2018", 
+          "status": "Open", 
+          "time": "12:00 PM-02:50 PM", 
+          "units": "  6.00", 
+          "wait_cap": "10", 
+          "wait_seats": "10"
+        }, 
+        {
+          "CRN": "40203", 
+          "campus": "FH", 
+          "course": "PHYS F004A01", 
+          "days": "F", 
+          "desc": "GENERAL PHYSICS (CALC)", 
+          "end": "06/29/2018", 
+          "instructor": "Cascarano", 
+          "room": "4502", 
+          "seats": "28", 
+          "start": "04/09/2018", 
+          "status": "Open", 
+          "time": "11:00 AM-11:50 AM", 
+          "units": "  6.00", 
+          "wait_cap": "10", 
+          "wait_seats": "10"
+        }
+    ]
+    print(Classie(classExample))
+
 
 if __name__ == '__main__':
     main()
