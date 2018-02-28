319db5e05b67c3186814cd51005d73db7fb7419a
Grond <grond66@riseup.net>
2018-02-27 16:04:45 -0800

    Commit Byron's latest code

--- scheduleLogic.py
+++ scheduleLogic.py
@@ -1,44 +1,52 @@
 import sectionClass
-
+import copy
 
 def isOverLapping(classArr, classArr2):
     newSchedule = []
     for section in classArr:
-        newSchedule += section.time
-    for section in classArr2:
-        newSchedule += section.time
+        newSchedule += section.times
+    newSchedule += classArr2.times
     newSchedule = sorted(newSchedule, key=lambda x: x[0])
-    print(newSchedule)
     for i in range(1, len(newSchedule) - 1):
         if(newSchedule[i-1][1] > newSchedule[i][0]):
             return True
         return False
 
 
-def findPossibleSchedule(schedule, otherClasses, possibleSchedules):
-    if len(otherClasses) <= 0:
+def findPossibleSchedule(schedule, otherCourses, possibleSchedules):
+    if len(otherCourses) <= 0:
+        print(schedule)
+        if schedule == False :
+            return False
         possibleSchedules.append(schedule)
         return True
-    print(schedule, "\n\n", otherClasses[0])
-    condition = isOverLapping([schedule], otherClasses[0])
-    if not condition:
-        return findPossibleSchedule(schedule + otherClasses[0],
-                otherClasses.pop(0))
-    else:
-        return False
+    for section in otherCourses[0]:
+        condition = isOverLapping(schedule, section)
+        if not condition:
+            print(type(schedule), type(section))
+            schedule = schedule.extend(section)
+            print(schedule)
+            findPossibleSchedule(schedule,
+            otherCourses[1:], possibleSchedules)
+        else:
+            findPossibleSchedule(False, [], [])
+    return
 
 
 def schedulize(parsedCoursesArray):
-    for course in parsedCoursesArray:
-        for section in course:
-            print(section.crn)
     possibleSchedules = []
-    print("Parsed Array:", parsedCoursesArray, "\n\n\n\n")
+    for course in parsedCoursesArray:
+        print("\n------New Course------\n")
+        for section in course:
+            print("Section CRN:", section)
+    print("\n\n")
     for section in parsedCoursesArray[0]:
-        findPossibleSchedule(section, parsedCoursesArray[1:],
-                possibleSchedules)
-    for section in possibleSchedules:
-        print(section, "\n")
+        sectionArr = []
+        sectionArr.append(section)
+        findPossibleSchedule(sectionArr, parsedCoursesArray[1:],
+                copy.deepcopy(possibleSchedules))
+    for schedule in possibleSchedules:
+        print("possible schdule ", schedule)
 
 
 # takes in array of sections and turns them into section classes
@@ -46,7 +54,7 @@ def buildSchedule(coursesArray):
     # next(iter(course.values())) helps with getting the value of the first key
     parsedCoursesArray = []
     coursesArray = sorted(coursesArray, key=lambda k: len(k.keys()))
-    print("Sorted List:\n") 
+    print("Sorted List:\n")
     for course in coursesArray:
         print(course, "\n")
         sectionsArray = []
@@ -54,4 +62,3 @@ def buildSchedule(coursesArray):
             sectionsArray.append(sectionClass.Section(value))
         parsedCoursesArray.append(sectionsArray)
     schedulize(parsedCoursesArray)
-
